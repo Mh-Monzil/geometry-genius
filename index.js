a36435083ed886931b802e887ed4be6cef07efde
Mh-Monzil <monzil246@gmail.com>
2024-02-14 12:45:51 +0600

    done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const calculateTriangle = document.getElementById("calculate-triangle");
-calculateTriangle.addEventListener('click', () => {
+// triangle 
+function calculateTriangle(){
     const baseInput = document.getElementById("base");
     const baseValue = parseFloat(baseInput.value);
 
@@ -12,4 +12,36 @@ calculateTriangle.addEventListener('click', () => {
     triangleResult.innerText = area;
     baseInput.value = '';
     heightInput.value = '';
-})
+}
+
+// rectangle 
+function calculateRectangle(){
+    const widthInput = document.getElementById("width");
+    const widthValue = parseFloat(widthInput.value);
+
+    const lengthInput = document.getElementById("length");
+    const lengthValue = parseFloat(lengthInput.value);
+
+    const area = widthValue * lengthValue;
+
+    const rectangleResult = document.getElementById("rectangle-result");
+    rectangleResult.innerText = area;
+    widthInput.value = '';
+    lengthInput.value = '';
+}
+
+// parallel 
+function calculateParallelogram(){
+    const parallelBaseInput = document.getElementById("parallelogram-base");
+    const parallelBaseValue = parseFloat(parallelBaseInput.value);
+
+    const parallelHeightInput = document.getElementById("parallelogram-height");
+    const parallelHeightValue = parseFloat(parallelHeightInput.value);
+
+    const area = parallelBaseValue * parallelHeightValue;
+
+    const parallelogramResult = document.getElementById("parallelogram-result");
+    parallelogramResult.innerText = area;
+    parallelBaseInput.value = '';
+    parallelHeightInput.value = '';
+}
